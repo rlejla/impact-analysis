729447b1a789bfb529c6566f45ac6c53af4fb552
laylaramic <lejla.ramic8@gmail.com>
2025-02-02 13:36:53 +0100

    simulation and results graphing

--- Project/simulation/urls.py
+++ Project/simulation/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import GuidelinesListView, SubmissionsListView
+from .views import GuidelinesListView, SubmissionsListView, SimulationView, GraphReportView
 
 urlpatterns = [
     path('guidelines/', GuidelinesListView.as_view(), name='guidelines'),
     path('submissions/', SubmissionsListView.as_view(), name='submissions'),
+    path('simulate/', SimulationView.as_view(), name='simulate'),
+    path('graphs/', GraphReportView.as_view(), name='graphs'),
 ]
